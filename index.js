5e80435d432458acae9f12a8461f504a0b6f2c5b
EB <e.bains@hotmail.co.uk>
2023-05-05 00:41:41 +0100

    Added starter code.

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
+//not sure if I needed to generate the packagejson file or to copy it from the starter code?
+
+//install inquirer Each project you'll need to run npm init -y and then npm install --save inquirer@^8.0.0
