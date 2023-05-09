00e159ccfb2494a227b951cfdb2adfae42842e43
EB <e.bains@hotmail.co.uk>
2023-05-09 15:30:01 +0100

    Added some comments to help organise the code and also questions to consider, added folder for README.md to be generated to, added headings to markdown file.

--- index.js
+++ index.js
@@ -1,12 +1,109 @@
+
+//Variables?? 
+
+
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// array of questions for user- is fat arrow not needed?
+//const questions = [];
 
-];
+//Does table of contents need a section?
+
+const userPromt = () =>
+
+inquirer.userPrompt ([
+
+{
+
+    type:'Input',
+    message:'Please enter the title of your proejct',
+    name:'Project title',
+
+},
+
+{
+
+    type:'Input',
+    message:'Please provide a description of your proejct',
+    name:'Description',
+    
+},
+
+{
+
+    type:'Input',
+    message:'What are the steps required to install your project?',
+    name:'installation',
+    
+},
+
+{
+
+    type:'Input',
+    message:'What is the purpose of your application?',//Provide instructions and examples for use. Include screenshots as needed.
+    name:'Useage',
+    
+},
+
+
+
+{
+
+    type:'Input/Select',
+    message:'Please enter/select the license you require',
+    name:'License',
+    
+},
+
+
+
+{
+
+    type:'Input',
+    message:'please provide details of any contributors',
+    name:'Contributors',// not sure what this refers too
+    
+},
+
+
+
+{
+
+    type:'Input',
+    message:'What testing is required for this application?',
+    name:'Tests',
+    
+},
+
+
+
+{
+
+    type:'test',
+    message:'test',
+    name:'Questions',
+    
+},
+
+
+
+//{- spare
+
+   // type:'test',
+   // message:'test',
+    //name:'test',
+    
+//},
+    
+
+    
+
+
+
+])
 
 // function to write README file
 function writeToFile(fileName, data) {
