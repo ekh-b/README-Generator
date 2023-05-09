c8c5a9303c6122262f7097f39f548f4d38982184
EB <e.bains@hotmail.co.uk>
2023-05-09 21:49:37 +0100

    Added template to markdown file.

--- index.js
+++ index.js
@@ -1,112 +1,110 @@
 
-//Variables?? 
+//Installation is require for users to use this readme generator:
+//Import and use the native fs module to read and write to the file system
+//Install 3rd party module inquirer using npm(node package manager)- used to build command ine apps
+//
+
+//install inquirer Each project you'll need to run npm init -y and then npm install --save inquirer@^8.0.0
+// app/ filename.md- creates the file in that apps folder
+// modules functions
+
+
+
+//Set variables- with the modules they require 
 
 
 const fs = require("fs");
-const path = require('path');
+const path = require('path');//requires the path module 
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user- is fat arrow not needed?
-//const questions = [];
 
 //Does table of contents need a section?
 
-const userPromt = () =>
+//const userPromt = () =>
 
-inquirer.userPrompt ([
-
-{
-
-    type:'Input',
-    message:'Please enter the title of your proejct',
-    name:'Project title',
-
-},
-
-{
-
-    type:'Input',
-    message:'Please provide a description of your proejct',
-    name:'Description',
-    
-},
-
-{
-
-    type:'Input',
-    message:'What are the steps required to install your project?',
-    name:'installation',
-    
-},
-
-{
-
-    type:'Input',
-    message:'What is the purpose of your application?',//Provide instructions and examples for use. Include screenshots as needed.
-    name:'Useage',
-    
-},
+//inquirer.userPrompt ([])
 
 
 
-{
 
-    type:'Input/Select',
-    message:'Please enter/select the license you require',
-    name:'License',
-    
-},
+//array of questions for user
 
+const questions = [
+    {
+        type: 'Input',
+        message: 'Please enter the title of your project',
+        name: 'Project title',
+    },
 
+    {
+        type: 'Input',
+        message: 'Please provide a description of your project',
+        name: 'Description',
+    },
 
-{
+    {
+        type: 'Input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
 
-    type:'Input',
-    message:'please provide details of any contributors',
-    name:'Contributors',// not sure what this refers too
-    
-},
-
-
-
-{
-
-    type:'Input',
-    message:'What testing is required for this application?',
-    name:'Tests',
-    
-},
-
-
-
-{
-
-    type:'test',
-    message:'test',
-    name:'Questions',
-    
-},
+    {
+        type: 'Input',
+        message: 'What is the purpose of your application?',//Provide instructions and examples for use. Include screenshots as needed.
+        name: 'Useage',
+    },
 
 
+    {
+        type: 'Input/Select',
+        message: 'Please enter/select the license you require',//need to give optitions- list them?
+        name: 'License',
+    },
 
-//{- spare
 
-   // type:'test',
-   // message:'test',
+    {
+        type: 'Input',
+        message: 'please provide details of any contributors',
+        name: 'Contributors',// not sure what this refers too
+    },
+
+
+    {
+        type: 'Input',
+        message: 'What testing is required for this application?',
+        name: 'Tests',
+    },
+
+    {
+        type: 'test',
+        message: 'test',
+        name: 'Questions',//not sure what message this needs to say
+    },
+
+
+
+    //{- spare
+
+    // type:'test',
+    // message:'test',
     //name:'test',
-    
-//},
-    
 
-    
+    //},
 
 
+];
 
-])
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+
+
+
+    
 }
 
 // function to initialize program
@@ -120,6 +118,3 @@ init();
 
 //not sure if I needed to generate the packagejson file or to copy it from the starter code?
 
-//install inquirer Each project you'll need to run npm init -y and then npm install --save inquirer@^8.0.0
-// app/ filename.md- creates the file in that apps folder
-// modules functions
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,38 @@
+
+//import this file into index.js
+
+// generate readme in another folder
+
+// make sue you 're in the project folder not the app folder
+
+
+# <Your-Project-Title>
+
+## Description
+
+
+
+
+## Table of Contents (Optional)
+
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation
+
+
+
+## Usage
+
+## Credits
+
+## License
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
@@ -8,8 +43,3 @@ function generateMarkdown(data) {
 module.exports = generateMarkdown;
 
 
-//import this file into index.js
-
-// generate readme in another folder
-
-// make sue you 're in the project folder not the app folder
